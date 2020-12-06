0a6e61908c45a27c9b8402d4de6bb2a3403d240c
bhanujggandhi <gandhibhanuj@gmail.com>
2020-12-07 05:17:48 +0530

    goals input/add setup

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+  const [goalsInput, setGoalsInput] = useState("");
+  const [goalsList, setGoalsList] = useState([]);
+
+  const changeTextHandler = (inputText) => {
+    setGoalsInput(inputText);
+  };
+
+  const addButtonHandler = () => {
+    setGoalsList((currentGoals) => [
+      ...currentGoals,
+      {
+        id: Math.random().toString(),
+        value: goalsInput,
+      },
+    ]);
+    setGoalsInput("");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder='Goals?'
+          value={goalsInput}
+          onChangeText={changeTextHandler}
+        />
+        <Button title='Add' onPress={addButtonHandler} />
+      </View>
+      <FlatList
+        keyExtractor={(item) => item.id}
+        data={goalsList}
+        renderItem={(itemData) => (
+          <TouchableOpacity
+            style={styles.listItem}
+            onPress={() => {
+              console.log("press");
+            }}
+          >
+            <Text>{itemData.item.value}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
   },
 });
